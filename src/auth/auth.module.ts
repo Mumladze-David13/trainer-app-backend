a212c575c70f29c1954270a4e07a5715e43062f2
Mumladze David <david.mumladze@lanit-tercom.ru>
2026-06-01 16:10:27 +0700

    fix: increase jwt expiry to 30d

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'trainer-app-secret-key',
-      signOptions: { expiresIn: '7d' },
+      signOptions: { expiresIn: '30d' },
     }),
   ],
   controllers: [AuthController],
